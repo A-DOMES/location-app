9b79f521336aab0e6f3212988c0fcec99c1b5114
A-DOMES <mailsock9@gmail.com>
2026-04-02 16:18:54 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,13 @@
-const CACHE_NAME = "adomes-cache-v4"; 
+const CACHE_NAME = "adomes-cache-v1"; 
 // ✅ 캐시 이름을 v4로 바꿔서 새 버전 적용
 
+const CACHE_NAME = "adomes-cache-v1";
 const urlsToCache = [
-  "/index.html",
-  "/client.html",       // ✅ 지도 페이지
-  "/user.html",         // ✅ 사용자 위치 전송 페이지
-  "/registerUser.html", // ✅ 사용자 등록 페이지
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png",
-  "/btn-current.png",   // ✅ 지도 버튼 이미지 추가
-  "/config.js"          // ✅ 새로 추가된 설정 파일
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png",
+  "config.js"   // ✅ 새로 추가된 파일
 ];
 
 // ✅ 설치 단계: 캐시에 필요한 파일들을 모두 저장
