077a6f87ea6db25ec5c08e0ecf73687b1998f409
A-DOMES <mailsock9@gmail.com>
2026-06-18 00:10:03 +0900

    Initialize OpenLayers map in map.js

---
+++ map.js
@@ -0,0 +1,13 @@
+// map.js
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([127.0, 37.5]),
+    zoom: 7
+  })
+});
