f0cd66d4735c45ce80b963e377c45f517debc772
A-DOMES <mailsock9@gmail.com>
2026-07-05 23:55:52 +0900

    Update vworld.js

--- api/vworld.js
+++ api/vworld.js
@@ -3,11 +3,18 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 
-app.use('/api/vworld', createProxyMiddleware({
-  target: 'https://api.vworld.kr',
+// ✅ WMS 프록시
+app.use('/api/vworld/req/wms', createProxyMiddleware({
+  target: 'https://api.vworld.kr/req/wms',
   changeOrigin: true,
-  pathRewrite: { '^/api/vworld': '' },
+  pathRewrite: { '^/api/vworld/req/wms': '' },
+}));
+
+// ✅ WMTS 프록시
+app.use('/api/vworld/req/wmts', createProxyMiddleware({
+  target: 'https://api.vworld.kr/req/wmts',
+  changeOrigin: true,
+  pathRewrite: { '^/api/vworld/req/wmts': '' },
 }));
 
 export default app;
-
