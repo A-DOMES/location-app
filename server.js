3e79f8d5eeab27de0a78188ad551ca6df4bfa3b0
A-DOMES <mailsock9@gmail.com>
2026-07-05 22:33:33 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-import express from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
 
-const app = express();
-
-app.use('/api/vworld', createProxyMiddleware({
-  target: 'https://api.vworld.kr',
-  changeOrigin: true,
-  pathRewrite: { '^/api/vworld': '' },
-}));
-
-export default app;
